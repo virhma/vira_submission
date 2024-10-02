eb4d82cdebaf5c668d4fe9adfa35960c903285c2
virhma <virarahmawati553@gmail.com>
2024-10-03 02:27:15 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,8 +4,8 @@ import seaborn as sns
 import streamlit as st
 
 #Load Data
-day_df = pd.read_csv("day.csv")
-hour_df = pd.read_csv("hour.csv")
+day_df = pd.read_csv("./vira_submission/data/day.csv")
+hour_df = pd.read_csv("./vira_submission/data/hour.csv")
 
 
 # DataFrame
